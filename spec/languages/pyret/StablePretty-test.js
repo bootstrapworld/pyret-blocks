74f659a91f711620a5c19bd1f4d5cc98f1d28025
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2019-04-26 16:55:42 -0400

    make work for one test
    could do the same to the others, including
    files in /src?

--- spec/languages/pyret/StablePretty-test.js
+++ spec/languages/pyret/StablePretty-test.js
@@ -1,4 +1,4 @@
-import CodeMirrorBlocks from 'codemirror-blocks';
+import CodeMirrorBlocks from '../../../node_modules/codemirror-blocks';
 import pyret from '../../../src/languages/pyret';
 import {store} from './store';
 import 'codemirror/addon/search/searchcursor.js';
