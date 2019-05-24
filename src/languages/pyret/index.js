a59df7a2c83539351ed667897979545fe4ae5eeb
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2019-05-24 10:51:11 -0400

    export function to create cmb with pyret

--- src/languages/pyret/index.js
+++ src/languages/pyret/index.js
@@ -1,18 +1,21 @@
 import PyretParser from './PyretParser';
+import CodeMirrorBlocks from "../../../node_modules/codemirror-blocks";
 require('./style.less');
 
-export default 
-  {
-    id: 'Pyret',
-    name: 'Pyret',
-    description: 'The Pyret language',
-    getParser() {
-      return new PyretParser();
-    },
-    getRenderOptions() {
-      return {
-        // TODO: perhaps also ['functionDefinition', 'variableDefinition', 'structDefinition']?
-        lockNodesOfType: ['comment']
-      };
-    },
-  };
+const language = {
+  id: 'Pyret',
+  name: 'Pyret',
+  description: 'The Pyret language',
+  getParser() {
+    return new PyretParser();
+  },
+  getRenderOptions() {
+    return {
+      // TODO: perhaps also ['functionDefinition', 'variableDefinition', 'structDefinition']?
+      lockNodesOfType: ['comment']
+    };
+  },
+};
+
+const constructor = (container, options) => new CodeMirrorBlocks(container, options, language);
+export default constructor;
