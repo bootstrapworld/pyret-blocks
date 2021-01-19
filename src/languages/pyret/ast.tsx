1be5d9dc9a9350f5c885aa3377dd7eaf00ef25f8
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-01-19 08:42:30 -0800

    Fix library function param errors

--- src/languages/pyret/ast.tsx
+++ src/languages/pyret/ast.tsx
@@ -596,18 +596,25 @@ export class FunctionApp extends AST.ASTNode {
   func: AST.ASTNode;
   args: AST.ASTNode[];
 	bgcClassName: string;
+	argsBgcClassNames: string[];
+	isLibFunc: boolean;
 
   constructor(from, to, func, args, bgcClassName, options={}) {
     super(from, to, 'funApp', options);
     this.func = func;
     this.args = args;
 		this.bgcClassName = bgcClassName;
+
+		this.argsBgcClassNames = options["argsBgcClassNames"];
+		this.isLibFunc = (options["argsBgcClassNames"] !== null);
   }
 
   static spec = Spec.nodeSpec([
     Spec.required('func'),
     Spec.list('args'),
     Spec.value('bgcClassName'),
+    Spec.value('argsBgcClassNames'),
+    Spec.value('isLibFunc'),
   ])
 
   longDescription(level) {
@@ -632,14 +639,27 @@ export class FunctionApp extends AST.ASTNode {
   }
 
   render(props) {
+		let args = [];
+		args.push(<DropTarget />);
+		this.args.forEach((value, index) => {
+			if (this.isLibFunc){
+				args.push(<span className={`${this.argsBgcClassNames[index]} funapp-params`}>{ value.reactElement({key: index}) }</span>);
+			}
+			else{
+				args.push(value.reactElement({key: index}));
+			}
+			args.push(<DropTarget />);
+		});
+
     return (
 			<span className={this.bgcClassName}>
 				<Node node={this} {...props}>
 					<span className="blocks-funapp">
 						<Args field="func">{[this.func]}</Args>
 					</span>
+
 					<span className="blocks-args">
-						<Args field="args">{this.args}</Args>
+						{ args }
 					</span>
 				</Node>
 			</span>
