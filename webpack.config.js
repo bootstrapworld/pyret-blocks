b4ac1e5124638e56f5e77a6b332e402c8c63635b
Paul Carduner <paul@carduner.net>
2021-08-10 11:17:51 -0700

    Switch to using devserver webpack config from codemirror-blocks toolkit

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
-module.exports = [
-  require('./webpack/example.config.js')
-].concat(require('./webpack/bundle.config.js'));
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  getWebpackDevServerConfig,
+  getWebpackBundleConfig,
+} = require('codemirror-blocks/lib/toolkit/webpack');
+
+const devServerConfig = getWebpackDevServerConfig({
+  context: path.resolve(__dirname, 'example'),
+  entry: './editor-example.js',
+});
+
+devServerConfig.module.rules.push({ test: /\.arr$/, use: 'raw-loader' });
+devServerConfig.plugins.push(new HtmlWebpackPlugin({
+  filename: 'editor.html',
+  template: 'editor.html',
+  inject: 'body',
+}));
+
+const bundleConfigs = require('./webpack/bundle.config');
+
+module.exports = [devServerConfig, 
+  ...bundleConfigs
+];
